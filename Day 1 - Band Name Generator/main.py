50c74569a2b7f6c5a0697dc5743b2fe469bed435
ar-yan9 <aryankumar6360@gmail.com>
2026-05-20 23:54:13 +0530

    Moved all projects and files into the ALL directory

--- Day 1 - Band Name Generator/main.py
+++
@@ -1,9 +0,0 @@
-# Day 1 - Band Name Generator
-
-print("Welcome to the Band Name Generator!")
-
-city = input("What's the name of the city you grew up in?\n")
-pet = input("What's your pet's name?\n")
-
-band_name = city + " " + pet
-print(f"Your band name could be: {band_name}")
